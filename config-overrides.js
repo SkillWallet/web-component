bdf8a0080f9f480ec24bbd66dfdd9490ceff48f6
taulant.disha <taulantdisha@Taulants-MacBook-Pro.local>
2021-12-20 16:45:25 +0200

    Added path alias in config

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+const { alias } = require('react-app-rewire-alias');
+
 module.exports = {
   webpack: (configuration) => {
     configuration.entry = './src/index.tsx';
@@ -6,7 +11,16 @@ module.exports = {
       (p) => p.constructor.name !== 'ManifestPlugin' && p.constructor.name !== 'MiniCssExtractPlugin'
     );
     delete configuration.optimization;
-    return configuration;
+    const modifiedConfig = alias({
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      '@mui/material': path.resolve('./node_modules/@mui/material'),
+      '@mui/icons-material': path.resolve('./node_modules/@mui/icons-material'),
+      '@emotion/react': path.resolve('./node_modules/@emotion/react'),
+      '@emotion/styled': path.resolve('./node_modules/@emotion/styled'),
+      'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+    })(configuration);
+    return modifiedConfig;
   },
   devServer: (configFunction) => {
     return (proxy, allowedHost) => {
