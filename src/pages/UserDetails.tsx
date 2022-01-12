2e36a97fe22050df778622765748b291533e5639
antGe <ant.iv.ge@gmail.com>
2022-01-12 17:57:54 +0200

    added file upload and react-hook-form

--- src/pages/UserDetails.tsx
+++ src/pages/UserDetails.tsx
@@ -4,16 +4,29 @@ import { SwButton } from 'sw-web-shared';
 import { Link } from 'react-router-dom';
 import { Box, Button, Input, TextField, Typography } from '@mui/material';
 import { ethers } from 'ethers';
-import { Form, Formik, isInputEvent } from 'formik';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
 import IPage from '../interfaces/page';
 import { currentCommunity, setLoading } from '../store/sw-auth.reducer';
 import { changeNetwork, fetchSkillWallet } from '../services/web3/web3Service';
 import RemainingCharsTextInput from '../components/RemainingCharsTextInput';
 
+interface Values {
+  file?: File;
+  text: string;
+}
+
 const UserDetails: React.FunctionComponent<IPage> = (props) => {
+  const { register, handleSubmit, formState } = useForm({
+    mode: 'onChange',
+  });
   const [inputIsValid, setInputIsValid] = useState(false);
   const dispatch = useDispatch();
   const community = useSelector(currentCommunity);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <Box
@@ -55,131 +68,108 @@ const UserDetails: React.FunctionComponent<IPage> = (props) => {
           alignItems: 'center',
         }}
       >
-        <Formik
-          initialValues={{ username: '', text: '' }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.username) {
-              // errors.email = 'Required';
-            } else if (values.username.length > 19) {
-              // errors.email = 'Invalid email address';
-            }
-            return errors;
-          }}
-          onSubmit={(values) => {
-            setTimeout(() => {
-              console.log(values);
-            }, 400);
-          }}
-        >
-          {({ values, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            sx={{
+              maxWidth: '382px',
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box sx={{ width: '382px', mb: '18px' }}>
+              <Typography variant="h4" sx={{ fontWeight: '400', textDecorationLine: 'underline' }}>
+                Nickname
+              </Typography>
+            </Box>
             <Box
               sx={{
-                maxWidth: '382px',
+                position: 'relative',
+                height: '68px',
                 display: 'flex',
+                alignContent: 'center',
                 justifyContent: 'center',
-                flexDirection: 'column',
-                alignItems: 'center',
+                border: 2,
+                borderColor: 'secondary',
+                mb: '18px',
+                px: '16px',
               }}
-              component={Form}
             >
-              <Box sx={{ width: '382px', mb: '18px' }}>
-                <Typography variant="h4" sx={{ fontWeight: '400', textDecorationLine: 'underline' }}>
-                  Nickname
-                </Typography>
-              </Box>
-              <Box
+              <Typography
                 sx={{
-                  position: 'relative',
-                  height: '68px',
-                  display: 'flex',
-                  alignContent: 'center',
-                  justifyContent: 'center',
+                  flex: 1,
+                  my: 'auto',
+                }}
+                variant="h4"
+              >
+                How do you want your community to call you?
+              </Typography>
+              <Input
+                sx={{
+                  my: 'auto',
+                  height: '40px',
+                  flex: 1,
                   border: 2,
                   borderColor: 'secondary',
-                  mb: '18px',
-                  px: '16px',
                 }}
-              >
-                <Typography
-                  sx={{
-                    flex: 1,
-                    my: 'auto',
-                  }}
-                  variant="h4"
-                >
-                  How do you want your community to call you?
-                </Typography>
-                <Input
-                  sx={{
-                    my: 'auto',
-                    height: '40px',
-                    flex: 1,
-                    border: 2,
-                    borderColor: 'secondary',
-                  }}
-                  type="username"
-                  name="username"
-                  onChange={handleChange}
-                  value={values.username}
-                />
-              </Box>
-              <Box sx={{ width: '382px', mb: '18px' }}>
-                <Typography variant="h3" sx={{ fontWeight: '400', textDecorationLine: 'underline' }}>
-                  Avatar
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  height: '96px',
-                  alignContent: 'center',
-                  justifyContent: 'center',
-                  display: 'flex',
-                  border: 2,
-                  borderColor: 'secondary',
-                  mb: '24px',
-                  px: '16px',
-                }}
-              >
-                <Typography
-                  sx={{
-                    flex: 1,
-                    my: 'auto',
-                  }}
-                  variant="h4"
-                >
-                  A public image - that’s how others will see you.
-                </Typography>
-                <Input
-                  sx={{
-                    my: 'auto',
-                    height: '40px',
-                    flex: 1,
-                    border: 2,
-                    borderColor: 'secondary',
-                  }}
-                  type="file"
-                  name="file"
-                  onChange={handleChange}
-                  value={values.username}
-                />
-              </Box>
-              <SwButton
-                sx={{
-                  whiteSpace: 'nowrap',
-                  borderColor: 'primary.main',
-                  height: '75px',
-                  maxWidth: '320px',
-                }}
-                mode="dark"
-                component={Button}
-                type="submit"
-                disabled={inputIsValid}
-                label="Next: Introduce yourself!"
+                type="text"
+                {...register('username', { required: true })}
               />
             </Box>
-          )}
-        </Formik>
+            <Box sx={{ width: '382px', mb: '18px' }}>
+              <Typography variant="h3" sx={{ fontWeight: '400', textDecorationLine: 'underline' }}>
+                Avatar
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                height: '96px',
+                alignContent: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+                border: 2,
+                borderColor: 'secondary',
+                mb: '24px',
+                px: '16px',
+              }}
+            >
+              <Typography
+                sx={{
+                  flex: 1,
+                  my: 'auto',
+                }}
+                variant="h4"
+              >
+                A public image - that’s how others will see you.
+              </Typography>
+              <Input
+                sx={{
+                  my: 'auto',
+                  height: '40px',
+                  flex: 1,
+                  border: 2,
+                  borderColor: 'secondary',
+                }}
+                {...register('picture', { required: true })}
+                type="file"
+              />
+            </Box>
+            <SwButton
+              sx={{
+                whiteSpace: 'nowrap',
+                borderColor: 'primary.main',
+                height: '75px',
+                maxWidth: '320px',
+              }}
+              mode="dark"
+              component={Button}
+              type="submit"
+              disabled={!formState.isValid}
+              label="Next: Introduce yourself!"
+            />
+          </Box>
+        </form>
       </Box>
     </Box>
   );
